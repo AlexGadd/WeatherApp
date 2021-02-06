718d8554cce9f01bc73f2fd28adeb363bb7530e4
AlexGadd <gadda023@gmail.com>
2021-02-07 11:49:15 +1300

    Moved private data to json file which is added to gitnore

--- forecast.js
+++ forecast.js
@@ -1,12 +1,8 @@
 //Variables
 let temp = 15
 let humidity = 30
-let APIKey = "99f7d052e2e8aecfb002645be3fb922d"
-let lattitude = -41.32538061503036
-let longditute = 174.78823373485412
-let inf = fetch("api.openweathermap.org/data/2.5/weather?lat=-41.32538061503036&lon=174.78823373485412&appid=99f7d052e2e8aecfb002645be3fb922d.json")
-
-var json = fetch('data.json')
+var jsonData = fetch('data.json')
+var jsonAPI = fetch("API.json")
 console.log(json)
 
 //Toggle functions which sets the output to either be temp or humidity depending on what is already displayed
